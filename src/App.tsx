3672da8a82d74283796355cfd618b00ab3e3897e
Teras43 <Brandcurtis43@gmail.com>
2022-02-12 19:25:50 -0700

    stashing

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,16 @@ import { Box } from "./components"
 
 function App() {
   return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[2.5, 0, 0]} />
-    </Canvas>
+    <>
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[2.5, 0, 0]} />
+      </Canvas>
+      <div>Testing!</div>
+    </>
   );
 }
 
